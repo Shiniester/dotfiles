879994cadb9d984f6416740f96647b92fa737bc2
Shiniester <donnie.pengyaodong@outlook.com>
2024-12-24 16:12:45 +0800

    config(nvim):blink.cmp
    remove signature of blink.cmp

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -1,20 +1,15 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    completion = {
-      menu = {
-        border = "rounded",
-      },
-      documentation = {
-        window = {
-          border = "rounded",
-        },
-      },
-      signature = {
-        window = {
-          border = "rounded",
-        },
-      },
-    },
-  },
+	"saghen/blink.cmp",
+	opts = {
+		completion = {
+			menu = {
+				border = "rounded",
+			},
+			documentation = {
+				window = {
+					border = "rounded",
+				},
+			},
+		},
+	},
 }
