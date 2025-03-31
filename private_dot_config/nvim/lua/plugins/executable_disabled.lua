367ca9f6af9aa89c4982f9db7650bd2fcd18de2a
Shiniester <donnie.pengyaodong@outlook.com>
2025-03-31 22:45:25 +0800

    chore(nvim): delete project.nvim

--- private_dot_config/nvim/lua/plugins/executable_disabled.lua
+++ private_dot_config/nvim/lua/plugins/executable_disabled.lua
@@ -1,5 +1,4 @@
 return {
   { "catppuccin", enabled = false },
   { "luozhiya/fittencode.nvim", enabled = false },
-  { "ahmedkhalf/project.nvim", enabled = false },
 }
--- private_dot_config/nvim/lua/plugins/executable_luasnip.lua
+++ private_dot_config/nvim/lua/plugins/executable_luasnip.lua
@@ -1,5 +1,5 @@
 local ls_loader = require("luasnip.loaders.from_lua")
-local snip_path = vim.fn.stdpath("config") .. "/lua/snippets"
+local snip_path = vim.fn.stdpath("config") .. "/snippets"
 return {
   "L3MON4D3/LuaSnip",
   event = "InsertEnter",
