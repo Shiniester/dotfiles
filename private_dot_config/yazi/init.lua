9e1193d6b96e0bef408a29a6713323518f5abb7c
Shiniester <donnie.pengyaodong@outlook.com>
2025-05-07 08:13:07 +0800

    chore(yazi):plugins:eza-preview,searchjump

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -10,3 +10,26 @@ require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
+
+require("eza-preview"):setup({
+	-- Determines the directory depth level to tree preview (default: 3)
+	level = 2,
+
+	-- Whether to follow symlinks when previewing directories (default: false)
+	follow_symlinks = false,
+})
+
+require("searchjump"):setup({
+	unmatch_fg = "#b2a496",
+	match_str_fg = "#000000",
+	match_str_bg = "#73AC3A",
+	first_match_str_fg = "#000000",
+	first_match_str_bg = "#73AC3A",
+	lable_fg = "#EADFC8",
+	lable_bg = "#BA603D",
+	only_current = false,
+	show_search_in_statusbar = false,
+	auto_exit_when_unmatch = false,
+	enable_capital_lable = true,
+	search_patterns = { "hell[dk]d", "%d+.1080p", "第%d+集", "第%d+话", "%.E%d+", "S%d+E%d+" },
+})
