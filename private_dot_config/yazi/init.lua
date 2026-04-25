e80af0222f79fae30192d478558abd3387a34f0c
Shiniester <donnie.pengyaodong@outlook.com>
2026-04-26 01:37:22 +0800

    feat(yazi): add open rules of image
    use imv to open image in unix by default

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -79,3 +79,38 @@ require("yatline"):setup({
 		},
 	},
 })
+
+require("eza-preview"):setup({
+	-- Set the tree preview to be default (default: true)
+	default_tree = true,
+
+	-- Directory depth level for tree preview (default: 3)
+	level = 2,
+
+	-- Show file icons
+	icons = true,
+
+	-- Follow symlinks when previewing directories (default: true)
+	follow_symlinks = true,
+
+	-- Show target file info instead of symlink info (default: false)
+	dereference = false,
+
+	-- Show hidden files (default: true)
+	all = false,
+
+	-- Ignore files matching patterns (default: {})
+	-- ignore_glob = "*.log"
+	-- ignore_glob = { "*.tmp", "node_modules", ".git", ".DS_Store" }
+	-- SEE: https://www.linuxjournal.com/content/pattern-matching-bash to learn about glob patterns
+	ignore_glob = {},
+
+	-- Ignore files mentioned in '.gitignore'  (default: true)
+	git_ignore = true,
+
+	-- Show git status (default: false)
+	git_status = false,
+})
+
+-- Or use default settings
+-- require("eza-preview"):setup({})
